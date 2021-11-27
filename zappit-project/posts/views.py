9c2139435a911d34a60de168bb14cded4c22ada2
rhaertel <renanhaertel@gmail.com>
2021-11-27 17:04:52 -0300

    changes post view to be loggin protected or read only

--- zappit-project/posts/views.py
+++ zappit-project/posts/views.py
@@ -7,5 +7,7 @@ from .serializers import PostSerializer
 class PostList(generics.ListCreateAPIView):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
     def perform_create(self, serializer):
         serializer.save(poster = self.request.user)
