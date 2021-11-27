2d255901d9ce0e11eb978148025ca175e4e7ef4c
rhaertel <renanhaertel@gmail.com>
2021-11-27 17:05:44 -0300

    Adds poster_id field Changes poster and poster_id to be readonly

--- zappit-project/posts/serializers.py
+++ zappit-project/posts/serializers.py
@@ -3,7 +3,10 @@ from .models import Post
 
 
 class PostSerializer(serializers.ModelSerializer):
+
+    poster = serializers.ReadOnlyField(source='poster.username')
+    poster_id = serializers.ReadOnlyField(source='poster.id')
     class Meta: 
         model = Post
-        fields = ['id', 'title', 'url', 'poster', 'created']
+        fields = ['id', 'title', 'url', 'poster', 'posterid', 'created']
   
