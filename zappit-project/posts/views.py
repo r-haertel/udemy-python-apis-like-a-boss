0bed3d98ea5126dc95856dd0d97358c49425e6d8
rhaertel <renanhaertel@gmail.com>
2021-11-27 17:04:09 -0300

    Adds post link to logged user

--- zappit-project/posts/views.py
+++ zappit-project/posts/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from rest_framework import generics
+from rest_framework import generics, permissions
 from .models import Post
 from .serializers import PostSerializer
 
@@ -7,3 +7,5 @@ from .serializers import PostSerializer
 class PostList(generics.ListCreateAPIView):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+    def perform_create(self, serializer):
+        serializer.save(poster = self.request.user)
