d53b91a1106b172ef66f5ffca10e0ac9439cdd7d
rhaertel <renanhaertel@gmail.com>
2021-11-28 09:48:15 -0300

    Adds delete logic for posts

--- zappit-project/posts/views.py
+++ zappit-project/posts/views.py
@@ -14,6 +14,18 @@ class PostList(generics.ListCreateAPIView):
     def perform_create(self, serializer):
         serializer.save(poster = self.request.user)
 
+class PostRetrieveDestroy(generics.RetrieveDestroyAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    def delete(self, request, *args, **kwargs): 
+        post = Post.objects.filter(pk=kwargs['pk'], poster=self.request.user)
+        if post.exists():
+            return self.destroy(request, *args, **kwargs)
+        else:
+            raise ValidationError('This isn\'t your post, don\'t try deleting other people stuff!')
+
 class VoteCreate(generics.CreateAPIView, mixins.DestroyModelMixin):
     serializer_class = VoteSerializer
     permission_classes = [permissions.IsAuthenticated]
@@ -34,3 +46,4 @@ class VoteCreate(generics.CreateAPIView, mixins.DestroyModelMixin):
             return Response(status=status.HTTP_200_OK)
         else:
             raise ValidationError('Vote doesn\'t exists')
+
